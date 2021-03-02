1a1e6d309ca415d328de89da7752283daa3ec621
Miguel <buguel2000@gmail.com>
2021-03-02 15:04:14 -0300

    footer

--- blog-miguel/components/footer.tsx
+++ blog-miguel/components/footer.tsx
@@ -3,11 +3,11 @@ import { EXAMPLE_PATH } from '../lib/constants'
 
 const Footer = () => {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="border-t ">
       <Container>
-        <div className="py-5 flex flex-col lg:flex-row items-center">
-          <h3 className=" lg:text-3xl font-bold tracking-tighter leading-tight text-center lg:mb-0">
-            Statically Generated with Next.js.
+        <div className="py-5 flex flex-col lg:flex-row items-center  ">
+          <h3 className=" text-2xl font-bold tracking-tighter leading-tight text-center lg:mb-0">
+            © 2021 Blog do Miguel 💻
           </h3>
         </div>
       </Container>
